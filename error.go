39b561f3e5a75f470fab916e4d32f336da90c97b
Salman Niazi <salman@logicalclocks.com>
2021-08-10 14:05:51 +0200

    Handling all HopsFS client Exceptions.

--- error.go
+++ error.go
@@ -11,6 +11,12 @@ const (
 	pathIsNotEmptyDirException   = "org.apache.hadoop.fs.PathIsNotEmptyDirectoryException"
 	fileAlreadyExistsException   = "org.apache.hadoop.fs.FileAlreadyExistsException"
 	alreadyBeingCreatedException = "org.apache.hadoop.hdfs.protocol.AlreadyBeingCreatedException"
+	invalidPathException         = "org.apache.hadoop.fs.InvalidPathException"
+	safeModeException            = "org.apache.hadoop.hdfs.server.namenode.SafeModeException"
+	dSQuotaExceededException     = "org.apache.hadoop.hdfs.protocol.DSQuotaExceededException"
+	nSQuotaExceededException     = "org.apache.hadoop.hdfs.protocol.NSQuotaExceededException"
+	parentNotDirectoryException  = "org.apache.hadoop.fs.ParentNotDirectoryException"
+	UnresolvedLinkException      = "org.apache.hadoop.fs.UnresolvedLinkException"
 )
 
 // Error represents a remote java exception from an HDFS namenode or datanode.
@@ -50,6 +56,18 @@ func interpretException(err error) error {
 		return syscall.ENOTEMPTY
 	case fileAlreadyExistsException:
 		return os.ErrExist
+	case invalidPathException:
+		return syscall.ENOENT
+	case safeModeException:
+		return syscall.EROFS
+	case dSQuotaExceededException:
+		return syscall.EDQUOT
+	case nSQuotaExceededException:
+		return syscall.EDQUOT
+	case parentNotDirectoryException:
+		return syscall.ENOENT
+	case UnresolvedLinkException:
+		return syscall.ENOLINK
 	default:
 		return err
 	}
